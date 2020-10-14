b213d434bea13c84b020c8b1319ddff4185d39b4
Sushil Mittal <sushilmittal@gmail.com>
2020-10-14 06:58:59 -0700

    Minor bug fix in train_test_splitter

--- preprocess/train_test_splitter.py
+++ preprocess/train_test_splitter.py
@@ -31,23 +31,25 @@ def split_dataset(name, data_path, test_frac):
                                            test_size=test_frac,
                                            random_state=0)
 
+    # Create train / test split assuming that n_splits=1.
     for train_index, test_index in dataset_split.split(data, labels):
         data_train, data_test = data[train_index], data[test_index]
         labels_train, labels_test = labels[train_index], labels[test_index]
 
-    # Write train data in h5py format.
-    train_path = train_dir + '/data4torch.h5'
-    hf_train = h5py.File(train_path, 'w')
-    hf_train.create_dataset('data', data=data_train)
-    hf_train.create_dataset('labels', data=labels_train)
+        # Write train data in h5py format.
+        train_path = train_dir + '/data4torch.h5'
+        hf_train = h5py.File(train_path, 'w')
+        hf_train.create_dataset('data', data=data_train)
+        hf_train.create_dataset('labels', data=labels_train)
 
-    # Write test data in h5py format.
-    test_path = test_dir + '/data4torch.h5'
-    hf_test = h5py.File(test_path, 'w')
-    hf_test.create_dataset('data', data=data_test)
-    hf_test.create_dataset('labels', data=labels_test)
+        # Write test data in h5py format.
+        test_path = test_dir + '/data4torch.h5'
+        hf_test = h5py.File(test_path, 'w')
+        hf_test.create_dataset('data', data=data_test)
+        hf_test.create_dataset('labels', data=labels_test)
 
-    print("Completed processing ", name, "....")
+        print("Completed processing for", name)
+        break
 
 
 dataset_names = ["MNIST",
